2cec012d247727ef4dd2827b66dcd0191a4523d8
nikhil sharma <nikhil.sharma3755@gmail.com>
2025-02-27 07:10:43 +0530

    update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(
 
 app.options("*", (req, res) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://mytube0.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
