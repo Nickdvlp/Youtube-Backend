66e5d28c88f86a2db57a87e0a933b9d48e2e7681
nikhil sharma <nikhil.sharma3755@gmail.com>
2025-02-27 07:14:33 +0530

    cors change in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mytube0.netlify.app",
     credentials: true,
   })
 );
